56037b4bc9c6c4cc8a9add9c57434fda890fdd57
= <oezkanyanikbas@gmail.com>
2019-11-11 14:28:29 +0100

    Removed 'Hello World' Added Login Component

---
+++ mevn-stack/src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <header>
+      <h1>Login</h1>
+    </header>
+    <form>
+      <label>Input Name</label>
+      <input type='text'>      
+    </form>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
