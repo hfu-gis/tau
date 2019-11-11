61cae9d8709d971b0d169f4c8f0b5eae07d3622b
= <oezkanyanikbas@gmail.com>
2019-11-11 20:48:13 +0100

    Removed expired code from Vue-Material

--- mevn-stack/src/main.js
+++ mevn-stack/src/main.js
@@ -1,16 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
-
-Vue.use(MdButton)
-Vue.use(MdContent)
-Vue.use(MdTabs)
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
