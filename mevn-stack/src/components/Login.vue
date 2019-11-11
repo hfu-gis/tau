9875eae02dee242098bf3fac1274e94c6a48eee0
= <oezkanyanikbas@gmail.com>
2019-11-11 14:39:37 +0100

    Minor changes to Login Component

--- mevn-stack/src/components/Login.vue
+++ mevn-stack/src/components/Login.vue
@@ -4,8 +4,17 @@
       <h1>Login</h1>
     </header>
     <form>
-      <label>Input Name</label>
-      <input type='text'>      
+      <label>E-Mail</label>
+      <input 
+        type='email'
+        name='email'
+        required>
+      <label>Passwort</label>
+      <input 
+        type='password'
+        name='password'
+        required>
+      <button type='submit'>Login</button>  
     </form>
   </main>
 </template>
@@ -19,4 +28,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  input { 
+    display: block;
+    margin: 0.5rem auto;
+  }
 </style>
