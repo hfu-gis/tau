73e4f842e14681e102dac3190956d43c8a72e68f
Jonathan-Schnee <48802338+Jonathan-Schnee@users.noreply.github.com>
2019-11-15 17:38:41 +0100

    Jonathan-Schnee created Stack overview

--- mevn-stack/src/router/index.js
+++ mevn-stack/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import Stack from '../views/Stack.vue'
 
 Vue.use(VueRouter)
 
@@ -23,6 +24,11 @@ const routes = [
     path: '/login',
     name: 'login',
     component: Login
+  },
+  {
+    path: '/stacks',
+    name: 'stacks',
+    component: Stack
   }
 ]
 
