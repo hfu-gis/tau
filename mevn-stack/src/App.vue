6bdd7eb9b9b417048c969afe117f783cc6e68600
= <oezkanyanikbas@gmail.com>
2019-11-17 19:17:29 +0100

    Removed unneeded code.

--- mevn-stack/src/App.vue
+++ mevn-stack/src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <v-app id="app">
     <NavBar/>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/login">Login</router-link>
     <router-view/>
   </v-app>
 </template>
