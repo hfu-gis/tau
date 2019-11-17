dedbdd02157c7e8009fa0ef9fd52f1a90c4e43a2
= <oezkanyanikbas@gmail.com>
2019-11-17 19:17:43 +0100

    Improved Navigation

--- mevn-stack/src/views/NavBar.vue
+++ mevn-stack/src/views/NavBar.vue
@@ -1,47 +1,46 @@
 <template>
     <nav>
-<!-- Toolbar Menu-->
-        <v-toolbar flat>
-            <v-row >
-                <v-img src ="https://upload.wikimedia.org/wikipedia/de/5/57/Hochschule_Furtwangen_HFU_logo.svg"
-                       max-width="150"
-                       max-height="100">
-                </v-img>
-                <v-toolbar-title class="text-uppercase grey--text">
-                    <span class="font-weight-light">StudyCards</span>
-                </v-toolbar-title>
-            </v-row>
-
-     <!--Navigations Button (BurgerMenu)-->
-            <v-btn class= "hidden-lg-and-up" color="#01905A" dark @click="drawer = !drawer">Menu</v-btn>
-    <!-- Dropdown-Menu -->
-            <v-menu
-                    v-model="value"
-                    :disabled="disabled"
-                    :absolute="absolute"
-                    :open-on-hover="openOnHover"
-                    :close-on-click="closeOnClick"
-                    :close-on-content-click="closeOnContentClick"
-                    :offset-x="offsetX"
-                    :offset-y="offsetY">
-                <template v-slot:activator="{ on }">
-                    <v-btn class="hidden-md-and-down" color="#01905A" dark v-on="on">
-                        Dropdown
-                    </v-btn>
-                </template>
-                <v-list>
-                    <v-list-item
-                            v-for="(item, index) in items" :key="index" router :to="item.route">
-                        <v-list-item-title v-text="item.text"></v-list-item-title>
-                    </v-list-item>
-                </v-list>
-            </v-menu>
-    <!--Profilbild-->
-            <v-avatar class="hidden-md-and-down">
-                <img src ="https://karrierebibel.de/wp-content/uploads/2017/02/Profilbild-Tipp-Laecheln.jpg">
-            </v-avatar>
+    <!-- Toolbar Menu-->
+    <v-toolbar color=''>
+        <v-app-bar-nav-icon 
+            color='#000' 
+            @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-spacer></v-spacer>
+        <v-btn icon color='#000'>
+            <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+        <!-- Dropdown-Menu -->
+        <v-menu
+                v-model="value"
+                :disabled="disabled"
+                :absolute="absolute"
+                :open-on-hover="openOnHover"
+                :close-on-click="closeOnClick"
+                :close-on-content-click="closeOnContentClick"
+                :offset-x="offsetX"
+                :offset-y="offsetY">
+            <template v-slot:activator="{ on }">
+                <v-btn class="mx-2" fab dark color="" x-small>
+                    <v-icon dark>mdi-plus</v-icon>
+                </v-btn>    
+                <v-btn rounded color="" dark v-on="on" small>Mehr</v-btn>
+            </template>
+            <v-list dense=true>
+                <v-list-item
+                    v-for="(item, index) in items" 
+                    :key="index" 
+                    router 
+                    :to="item.route">
+                    <v-list-item-title v-text="item.text"></v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+        <!--Profilbild-->
+        <v-avatar class="" size="36">
+            <img src ="https://karrierebibel.de/wp-content/uploads/2017/02/Profilbild-Tipp-Laecheln.jpg">
+        </v-avatar>
         </v-toolbar>
-<!-- Navigation -->
+        <!-- Navigation -->
         <v-navigation-drawer app v-model="drawer" class="#01905A"  dark>
           <v-list flat>
               <v-list-item-group v-model="item" color="white">
@@ -53,7 +52,7 @@
               </v-list-item-group>
           </v-list>
         </v-navigation-drawer>
-<!--END-->
+    <!--END-->
     </nav>
 </template>
 
@@ -67,7 +66,7 @@
                     {text: 'Profil', route: '/'},
                     {text: 'Stapel erstellen', route: '/'},
                     {text: 'Meine Stapel', route: '/'},
-                    {text: 'Suche', route: '/search'},
+                    {text: 'Suche', route: '/'},
                     {text: 'Einstellungen', route: '/'},
                 ],
                 disabled: false,
