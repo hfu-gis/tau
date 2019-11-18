9d43e518f8b478906ba63777e0427de12468d6a4
= <oezkanyanikbas@gmail.com>
2019-11-18 12:36:38 +0100

    Navigation Re-Design

--- mevn-stack/src/App.vue
+++ mevn-stack/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <v-app id="app">
     <NavBar/>
-    <router-view/>
   </v-app>
 </template>
 
--- mevn-stack/src/views/NavBar.vue
+++ mevn-stack/src/views/NavBar.vue
@@ -1,87 +1,74 @@
 <template>
     <nav>
-    <!-- Toolbar Menu-->
-    <v-toolbar color=''>
-        <v-app-bar-nav-icon
-            class='d-flex d-sm-none' 
-            color='#000' 
-            @click="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-spacer></v-spacer>
-        <v-btn icon color='#000'>
-            <v-icon>mdi-magnify</v-icon>
-        </v-btn>
-        <!-- Dropdown-Menu -->
-        <v-menu
-                v-model="value"
-                :disabled="disabled"
-                :absolute="absolute"
-                :open-on-hover="openOnHover"
-                :close-on-click="closeOnClick"
-                :close-on-content-click="closeOnContentClick"
-                :offset-x="offsetX"
-                :offset-y="offsetY">
-            <template v-slot:activator="{ on }">
-                <v-btn class="mx-2" fab dark color="" x-small>
-                    <v-icon dark>mdi-plus</v-icon>
-                </v-btn>    
-                <v-btn rounded color="" dark v-on="on" small>Mehr</v-btn>
+        <v-navigation-drawer
+            v-model="drawer"
+            app
+            right>
+            
+            <template v-slot:prepend>
+                <v-list-item two-line>
+                    <v-list-item-avatar>
+                    <img src="https://randomuser.me/api/portraits/women/81.jpg">
+                    </v-list-item-avatar>
+
+                    <v-list-item-content>
+                    <v-list-item-title>Jane Smith</v-list-item-title>
+                    <v-list-item-subtitle>Logged In</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
             </template>
-            <v-list dense=true>
-                <v-list-item
-                    v-for="(item, index) in items" 
-                    :key="index" 
-                    router 
-                    :to="item.route">
-                    <v-list-item-title v-text="item.text"></v-list-item-title>
+
+            <v-divider></v-divider>
+            <v-list dense>
+                <v-list-item v-for="item in items" :key="item.text" link>
+                    <v-list-item-action>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-action>
+
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.text }}</v-list-item-title>
+                    </v-list-item-content>
                 </v-list-item>
             </v-list>
-        </v-menu>
-        <!--Profilbild-->
-        <v-avatar class="" size="36">
-            <img src ="https://karrierebibel.de/wp-content/uploads/2017/02/Profilbild-Tipp-Laecheln.jpg">
-        </v-avatar>
-        </v-toolbar>
-        <!-- Navigation -->
-        <v-navigation-drawer app v-model="drawer" class="#01905A"  dark>
-          <v-list flat>
-              <v-list-item-group v-model="item" color="white">
-                  <v-list-item v-for="(item, i) in items" :key="i" router :to="item.route">
-                      <v-list-item-content>
-                          <v-list-item-title v-text="item.text"></v-list-item-title>
-                      </v-list-item-content>
-                  </v-list-item>
-              </v-list-item-group>
-          </v-list>
         </v-navigation-drawer>
-    <!--END-->
+
+        <v-app-bar
+            app
+            color="primary"
+            dark>
+            <v-toolbar-title>App</v-toolbar-title>
+            <v-spacer />
+            <v-list-item-action
+                class='d-none d-sm-flex'>
+                <v-icon>mdi-account</v-icon>
+            </v-list-item-action>
+            <v-app-bar-nav-icon 
+                @click.stop="drawer = !drawer"
+                class='d-flex d-sm-none'/>
+        </v-app-bar>
+
+        <v-content>
+            <router-view/>
+        </v-content>
     </nav>
 </template>
 
 <script>
     export default {
-        name: "NavBar",
-        data(){
-            return {
-                drawer: false,
-                items: [
-                    {text: 'Login', route: '/auth/login'},
-                    {text: 'Landing', route: '/'},
-                    {text: 'Create',  route: '/deck/create'},
-                    {text: 'Decks',   route: '/deck/'},
-                    {text: 'About',   route: '/about'},
-                    {text: 'Profile', route: '/user/profile'},
-                ],
-                disabled: false,
-                absolute: false,
-                openOnHover: false,
-                value: false,
-                closeOnClick: true,
-                closeOnContentClick: true,
-                offsetX: false,
-                offsetY: true,
-            }
-        }
-
+        name: 'NavBar',
+        props: {
+            source: String,
+        },
+        data: () => ({
+            drawer: false,
+            items: [
+                { icon: 'mdi-account', text: 'Most Popular' },
+                { icon: 'mdi-account', text: 'Subscriptions' },
+                { icon: 'mdi-account', text: 'History' },
+                { icon: 'mdi-account', text: 'Playlists' },
+                { icon: 'mdi-account', text: 'Watch Later' },
+            ],
+        }),
     }
 </script>
 
