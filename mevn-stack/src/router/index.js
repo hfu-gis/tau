cebbedacfc566b9c0a599d1c9097d258da928e66
TroubleBeast <oezkanyanikbas@gmail.com>
2019-11-21 18:31:50 +0100

    Minor changes in Routing

--- mevn-stack/src/router/index.js
+++ mevn-stack/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 
 import Landing    from '@/components/Landing.vue'
 import Home       from '@/components/Home.vue'
+//import Stacks      from '@/components/Stacks.vue'
 import Stack      from '@/components/Stack.vue'
 import Search     from '@/components/Search'
 import Login      from '@/components/Login.vue'
@@ -10,25 +11,27 @@ import Login      from '@/components/Login.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', name: 'landing',   component: Landing },
-  { path: '/search',         name: 'search',    component: Search },
+  { path: '/', redirect: 'home', component: Landing },
+  { path: '/search', name:'search', component: Search },
 
-  { path: '/stack/all',             name: 'stacks', component: Landing },
-  { path: '/stack/:id',             name: 'stack',  component: Landing },
-  { path: '/stack/:id/add',         name: 'add',    component: Landing },
-  { path: '/stack/:id/edit/:id-c',  name: 'edit',   component: Landing },
-  { path: '/stack/:id/learn/:id-c', name: 'learn',  component: Landing },
-
-  { path: '/user/profile',   name: 'profile',   component: Landing },
-  { path: '/user/settings',  name: 'landing',   component: Landing },
-  { path: '/user/dashboard', name: 'landing',   component: Landing },
-
-  { path: '/auth/login',     name: 'login',     component: Login },
+  { path: '/stacks/',                name:'stacks',   component: Home, },
+  { path: '/stacks/:id',             name:'stack',    component: Stack },
+  { path: '/stacks/add',             name:'s-create', component: Home },
+  { path: '/stacks/:id/add/card',    name:'c-create', component: Home },
+  { path: '/stacks/:id/edit/:id-c',  name:'s-edit',   component: Home },
+  { path: '/stacks/:id/learn/:id-c', name:'s-learn',  component: Home },
   
-  { path: '/home',           name: 'home',      component: Home },
-  { path: '/about',          name: 'about',     component: () => import('@/components/About.vue') },
+
+  { path: '/user/profile',   component: Home },
+  { path: '/user/settings',  component: Home },
+  { path: '/user/dashboard', component: Home },
+
+  { path: '/auth/login', component: Login },
   
-  { path: '/stacks',         name: 'stacks',    component: Stack },
+  { path: '/home', name:'home', component: Home },
+  { path: '/about', name: 'about',     component: () => import('@/components/About.vue') },
+  
+  { path: '/stacks', component: Stack },
 ]
 
 const router = new VueRouter({
