7976684edd1ae22a8ba9a39ea6923716f32d2f43
TroubleBeast <oezkanyanikbas@gmail.com>
2019-11-21 18:31:35 +0100

    Created Stack Showcase /stacks/:id

--- mevn-stack/src/components/Home.vue
+++ mevn-stack/src/components/Home.vue
@@ -21,6 +21,7 @@
             </v-card>
             </v-dialog>
         </v-row>
+
         <section 
             v-for="(section, index) in sections" 
             :key="index">
@@ -41,16 +42,24 @@
                     <v-btn color="primary" depressed rounded small dark>
                         Lernen
                     </v-btn>
-                    <v-btn v-if="section == sections[0]" color="warning" depressed rounded small dark>
-                        Bearbeiten
-                    </v-btn>
+                    <v-btn 
+                        v-if="section == sections[0]" 
+                        color="warning" 
+                        class='ml-2' 
+                        depressed 
+                        rounded 
+                        small 
+                        dark 
+                        link 
+                        :to="'/stacks/' + item.id">Bearbeiten</v-btn>
                     <v-btn color="blue" fab depressed x-small dark @click.stop="dialog = true">
                         <v-icon>mdi-account-multiple</v-icon>
                     </v-btn>
                 </v-expansion-panel-content>
                 </v-expansion-panel>
             </v-expansion-panels>  
-        </section>        
+        </section>
+
     </main>
 </template>
 
@@ -64,8 +73,8 @@
                     {
                         title: "Meine Liste",
                         items: [
-                            {title: "Grundlagen interaktiver Systeme"},
-                            {title: "Computergrafik"}
+                            {title: "Grundlagen interaktiver Systeme", id:'123'},
+                            {title: "Computergrafik", id:'456'}
                         ]
                     }, 
                     {
@@ -87,32 +96,32 @@
 
 <style scoped>
     main {
-    width: 90%;
-    margin: 0 auto;
-  }
-  
-  @media (min-width: 600px) {
-    main { width: 60%; }
-  }
+        width: 90%;
+        margin: 0 auto;
+    }
+    
+    @media (min-width: 600px) {
+        main { width: 60%; }
+    }
 
-  @media (min-width: 960px) {
-    main { width: 40%; }
-  }
+    @media (min-width: 960px) {
+        main { width: 40%; }
+    }
 
-  @media (min-width: 1264px) {
-    main { width: 30%; }
-  }
+    @media (min-width: 1264px) {
+        main { width: 30%; }
+    }
 
-  @media (min-width: 1904px) {
-    main { width: 20%; }
-  }
+    @media (min-width: 1904px) {
+        main { width: 20%; }
+    }
 
-  button {
-      margin-left: 0.5rem;
-  }
+    button {
+        margin-left: 0.5rem;
+    }
 
-  .v-sheet--offset {
-    top: -24px;
-    position: relative;
-  }
+    .v-sheet--offset {
+        top: -24px;
+        position: relative;
+    }
 </style>
--- mevn-stack/src/components/Stack.vue
+++ mevn-stack/src/components/Stack.vue
@@ -1,138 +1,196 @@
 <template>
-    <div class="stacks" >
-    <h1 class="display 2">1.Semester</h1>
-    <v-layout row wrap>
-        <v-flex xs12 md6 lg3 class="my-2">
-             <v-card elevation="4" class="mx-auto green" width="300" height="200">
-                <v-card-text class="pb-0">
-                    <p class="display-2 white--text">AUDIOTECH</p>
-                    <p>200 Cards</p>
-                </v-card-text>
-                <v-card-actions>
-                    <v-layout row wrap align-center>
-                        <v-flex class="text-xs-center">
-                            <v-btn elevation="3" class='ma-3 green darken-3 white--text'>Ansehen</v-btn>
-                        </v-flex>
-                    </v-layout>
-                </v-card-actions>
-            </v-card>   
-        </v-flex>
+    <main class='mt-3'>
+        <v-container class='text-left'>
+            <v-card>
+                <v-data-table
+                    :headers="headers"
+                    :items="desserts"
+                    sort-by="calories"
+                    class="elevation-1"
+                >
+                    <template v-slot:top>
+                    <v-toolbar flat color="white">
+                        <v-toolbar-title>Karten bearbeiten</v-toolbar-title>
+                        <v-spacer></v-spacer>
+                        <v-dialog v-model="dialog" max-width="500px">
+                        <template v-slot:activator="{ on }">
+                            <v-btn color="primary" small depressed rounded dark class="mb-2" v-on="on">Neue Karte</v-btn>
+                        </template>
+                        <v-card>
+                            <v-card-title>
+                            <span class="headline">{{ formTitle }}</span>
+                            </v-card-title>
 
-        <v-flex xs12 md6 lg3 class="my-2">
-             <v-card elevation="4" class="mx-auto green" width="300" height="200">
-                <v-card-text class="pb-0">
-                    <p class="display-2 white--text">MATHE</p>
-                    <p>200 Cards</p>
-                </v-card-text>
-                <v-card-actions>
-                    <v-layout row wrap align-center>
-                        <v-flex class="text-xs-center">
-                            <v-btn elevation="3" class='ma-3 green darken-3 white--text'>Ansehen</v-btn>
-                        </v-flex>
-                    </v-layout>
-                </v-card-actions>
-            </v-card>
-        </v-flex>
+                            <v-card-text>
+                            <v-container>
+                                <v-row>
+                                <v-col cols="12">
+                                    <v-text-field v-model="editedItem.title" label="Titel"></v-text-field>
+                                </v-col>
+                                <v-col cols="12">
+                                    <v-text-field v-model="editedItem.front" label="Front"></v-text-field>
+                                </v-col>
+                                <v-col cols="12">
+                                    <v-text-field v-model="editedItem.back" label="Back"></v-text-field>
+                                </v-col>
+                                </v-row>
+                            </v-container>
+                            </v-card-text>
 
-        <v-flex xs12 md6 lg3 class="my-2">
-             <v-card elevation="4" class="mx-auto green" width="300" height="200">
-                <v-card-text class="pb-0">
-                    <p class="display-2 white--text">PROG</p>
-                    <p>200 Cards</p>
-                </v-card-text>
-                <v-card-actions>
-                    <v-layout row wrap align-center>
-                        <v-flex class="text-xs-center">
-                            <v-btn elevation="3" class='ma-3 green darken-3 white--text'>Ansehen</v-btn>
-                        </v-flex>
-                    </v-layout>
-                </v-card-actions>
+                            <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="blue darken-1" text @click="close">Schließen</v-btn>
+                            <v-btn color="blue darken-1" text @click="save">Speichern</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                        </v-dialog>
+                    </v-toolbar>
+                    </template>
+                    <template v-slot:item.action="{ item }">
+                    <v-icon
+                        color='warn'
+                        class="mr-2"
+                        @click="editItem(item)"
+                    >mdi-pencil
+                    </v-icon>
+                    <v-icon
+                        color='error'
+                        @click="deleteItem(item)"
+                    >mdi-delete</v-icon>
+                    </template>
+                    <template v-slot:no-data>
+                    <v-btn color="primary" @click="initialize">Reset</v-btn>
+                    </template>
+                </v-data-table>
             </v-card>
-        </v-flex>
 
-        <v-flex xs12 md6 lg3 class="my-2">
-             <v-card elevation="4" class="mx-auto green" width="300" height="200">
-                <v-card-text class="pb-0">
-                    <p class="display-2 white--text">PHYSIK</p>
-                    <p>200 Cards</p>
-                </v-card-text>
-                <v-card-actions>
-                    <v-layout row wrap align-center>
-                        <v-flex class="text-xs-center">
-                            <v-btn elevation="3" class='ma-3 green darken-3 white--text'>Ansehen</v-btn>
-                        </v-flex>
-                    </v-layout>
-                </v-card-actions>
+            <v-card class='mt-4 pa-4 text-center'>
+                <v-btn small rounded depressed dark color='primary'>Lernen</v-btn>
+                <v-btn small rounded depressed dark color='error'>Löschen</v-btn>
             </v-card>
-        </v-flex>
-    </v-layout>
-    
-    
-    <h1 class="display 2">2.Semester</h1>
-    <v-layout row wrap>
-        <v-flex xs12 md6 lg3 class="my-2">
-             <v-card elevation="4" class="mx-auto green" width="300" height="200">
-                <v-card-text class="pb-0">
-                    <p class="display-2 white--text">GIS</p>
-                    <p>200 Cards</p>
-                </v-card-text>
-                <v-card-actions>
-                    <v-layout row wrap align-center>
-                        <v-flex class="text-xs-center">
-                            <v-btn elevation="3" class='ma-3 green darken-3 white--text'>Ansehen</v-btn>
-                        </v-flex>
-                    </v-layout>
-                </v-card-actions>
-            </v-card>   
-        </v-flex>
-
-        <v-flex xs12 md6 lg3 class="my-2">
-             <v-card elevation="4" class="mx-auto green" width="300" height="200">
-                <v-card-text class="pb-0">
-                    <p class="display-2 white--text">MATHE</p>
-                    <p>200 Cards</p>
-                </v-card-text>
-                <v-card-actions>
-                    <v-layout row wrap align-center>
-                        <v-flex class="text-xs-center">
-                            <v-btn elevation="3" class='ma-3 green darken-3 white--text'>Ansehen</v-btn>
-                        </v-flex>
-                    </v-layout>
-                </v-card-actions>
-            </v-card>
-        </v-flex>
-
-        <v-flex xs12 md6 lg3 class="my-2">
-             <v-card elevation="4" class="mx-auto green" width="300" height="200">
-                <v-card-text class="pb-0">
-                    <p class="display-2 white--text">UX-DESIGN</p>
-                    <p>200 Cards</p>
-                </v-card-text>
-                <v-card-actions>
-                    <v-layout row wrap align-center>
-                        <v-flex class="text-xs-center">
-                            <v-btn elevation="3" class='ma-3 green darken-3 white--text'>Ansehen</v-btn>
-                        </v-flex>
-                    </v-layout>
-                </v-card-actions>
-            </v-card>
-        </v-flex>
-
-        <v-flex xs12 md6 lg3 class="my-2">
-             <v-card elevation="4" class="mx-auto green" width="300" height="200">
-                <v-card-text class="pb-0">
-                    <p class="display-2 white--text">CD</p>
-                    <p>200 Cards</p>
-                </v-card-text>
-                <v-card-actions>
-                    <v-layout row wrap align-center>
-                        <v-flex class="text-xs-center">
-                            <v-btn elevation="3" class='ma-3 green darken-3 white--text'>Ansehen</v-btn>
-                        </v-flex>
-                    </v-layout>
-                </v-card-actions>
-            </v-card>
-        </v-flex>
-    </v-layout>
-    </div>
+        </v-container>
+    </main>
 </template>
+
+<script>
+export default {
+    data: () => ({
+      dialog: false,
+      headers: [
+        {
+          text: 'Kartentitel',
+          align: 'left',
+          value: 'title',
+        },
+        { text: 'Gesehen', value: 'views', align: 'right', },
+        { text: 'Actions', value: 'action', sortable: false, align: 'right', },
+      ],
+      desserts: [],
+      editedIndex: -1,
+      editedItem: {
+        title: '',
+        front: '',
+        back: ''
+      },
+      defaultItem: {
+        title: '',
+        front: '',
+        back: ''
+      },
+    }),
+
+    computed: {
+      formTitle () {
+        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+      },
+    },
+
+    watch: {
+      dialog (val) {
+        val || this.close()
+      },
+    },
+
+    created () {
+      this.initialize()
+    },
+
+    methods: {
+      initialize () {
+        this.desserts = [
+          {
+            title: 'Erstellen Vue App',
+            front: '',
+            back: '',
+            views: 159,
+          },
+          {
+            title: 'Erstellen Vue Router',
+            front: '',
+            back: '',
+            views: 159,
+          }
+        ]
+      },
+
+      editItem (item) {
+        this.editedIndex = this.desserts.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+
+      deleteItem (item) {
+        const index = this.desserts.indexOf(item)
+        confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
+      },
+
+      close () {
+        this.dialog = false
+        setTimeout(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        }, 300)
+      },
+
+      save () {
+        if (this.editedIndex > -1) {
+          Object.assign(this.desserts[this.editedIndex], this.editedItem)
+        } else {
+          this.desserts.push(this.editedItem)
+        }
+        this.close()
+      },
+    },
+  }
+</script>
+
+<style scoped>
+    .v-card {
+        width: 100%;
+    }
+    main {
+        width: 90%;
+        margin: 0 auto;
+    }
+
+    @media (min-width: 960px) {
+        main { width: 80%; }
+    }
+
+    @media (min-width: 1264px) {
+        main { width: 60%; }
+    }
+
+    @media (min-width: 1904px) {
+        main { width: 40%; }
+    }
+
+    button {
+        margin-left: 0.5rem;
+    }
+
+    .v-sheet--offset {
+        top: -24px;
+        position: relative;
+    }
+</style>
