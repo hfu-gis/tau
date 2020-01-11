1f73be1bb590bf38fa01042b98825c720a6a88f8
TroubleBeast <oezkanyanikbas@gmail.com>
2020-01-11 15:48:04 +0100

    Add Cards to Stack-View

--- mevn-stack/src/components/Stack.vue
+++ mevn-stack/src/components/Stack.vue
@@ -7,77 +7,28 @@
 			<v-card class='pa-4 mb-4'>
 				<p>Titel: {{ stack.title }}</p>
 				<p>Beschreibung: {{ stack.description }}</p>
-                <v-icon>mdi-cards</v-icon> {{ stack.cards }}
+                <p><v-icon>mdi-cards</v-icon> {{ stack.cards.length }}</p>
 			</v-card>
-            <v-card>
-                <v-data-table
-                    :headers="headers"
-                    :items="cards"
-                    sort-by="calories"
-                    class="elevation-1"
-                >
-                    <template v-slot:top>
-                    <v-toolbar flat color="white">
-                        <v-toolbar-title>Karten bearbeiten</v-toolbar-title>
-                        <v-spacer></v-spacer>
-                        <v-dialog v-model="dialog" max-width="500px">
-                        <template v-slot:activator="{ on }">
-                            <v-btn 
-                                color="primary"
-                                small
-                                depressed
-                                rounded
-                                dark
-                                class="mb-2" 
-                                @click='createStack()'>Neue Karte</v-btn>
-                        </template>
-                        <v-card>
-                            <v-card-title>
-                            <span class="headline">{{ formTitle }}</span>
-                            </v-card-title>
+            
+            <v-btn 
+                color="primary"
+                small
+                rounded
+                dark
+                class="mb-4" 
+                @click='createStack()'>Neue Karte</v-btn>
 
-                            <v-card-text>
-                            <v-container>
-                                <v-row>
-                                <v-col cols="12">
-                                    <v-text-field v-model="editedItem.title" label="Titel"></v-text-field>
-                                </v-col>
-                                <v-col cols="12">
-                                    <v-text-field v-model="editedItem.front" label="Front"></v-text-field>
-                                </v-col>
-                                <v-col cols="12">
-                                    <v-text-field v-model="editedItem.back" label="Back"></v-text-field>
-                                </v-col>
-                                </v-row>
-                            </v-container>
-                            </v-card-text>
-
-                            <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <v-btn color="blue darken-1" text @click="close">Schließen</v-btn>
-                            <v-btn color="blue darken-1" text @click="save">Speichern</v-btn>
-                            </v-card-actions>
-                        </v-card>
-                        </v-dialog>
-                    </v-toolbar>
-                    </template>
-                    <template v-slot:item.action="{ item }">
-                    <v-icon
-                        color='warn'
-                        class="mr-2"
-                        @click="editItem(item)"
-                    >mdi-pencil
-                    </v-icon>
-                    <v-icon
-                        color='error'
-                        @click="deleteItem(item)"
-                    >mdi-delete</v-icon>
-                    </template>
-                    <template v-slot:no-data>
-                    <v-btn color="primary" @click="initialize">Reset</v-btn>
-                    </template>
-                </v-data-table>
-            </v-card>
+            <!-- Cards -->
+            <section>
+                <v-card class='pa-4'>
+                    <p class='subtitle-1'>Karten</p>
+                    <v-divider class='mb-4'></v-divider>
+                    <!-- Each Card -->
+                    <article v-for='(card, index) in stack.cards' :key='index'>
+                        <p>{{ index }} - {{ card.mapValue.fields.question.stringValue.substring(0, 50) }}...</p>
+                    </article>
+                </v-card>
+            </section>
 
             <v-card class='mt-4 pa-4 text-center'>
                 <v-btn small rounded depressed dark color='primary'>Lernen</v-btn>
@@ -100,38 +51,12 @@ export default {
 			err:       false,
 			errMsg:    "",
 			stack,
-
-			desserts:    "[]",
-			editedIndex: "-1",
-			editedItem: {
-				title: "",
-				front: "",
-				back:  ""
-			},
-			defaultItem: {
-				title: "",
-				front: "",
-				back:  ""
-			}
 		}
 	},
-
-    computed: {
-      formTitle () {
-        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
-      },
-    },
-
-    watch: {
-      dialog (val) {
-        val || this.close()
-      },
-    },
 
     created () {
 		this.isLoading = true
 		this.loadStack()
-		this.initialize()
     },
 
     methods: {
@@ -155,44 +80,12 @@ export default {
 					this.stack = {
 						title:       data.title.stringValue,
                         description: data.description.stringValue,
-                        cards: arr.length
+                        cards:       arr
 					}
 					this.isLoading = false
 				}
 			})
-		},
-
-		initialize () {
-			this.cards = stack.cards
-		},
-
-		editItem (item) {
-			this.editedIndex = this.cards.indexOf(item)
-			this.editedItem = Object.assign({}, item)
-			this.dialog = true
-		},
-
-		deleteItem (item) {
-			const index = this.cards.indexOf(item)
-			confirm('Are you sure you want to delete this item?') && this.cards.splice(index, 1)
-		},
-
-		close () {
-			this.dialog = false
-			setTimeout(() => {
-			this.editedItem = Object.assign({}, this.defaultItem)
-			this.editedIndex = -1
-			}, 300)
-		},
-
-		save () {
-			if (this.editedIndex > -1) {
-			Object.assign(this.cards[this.editedIndex], this.editedItem)
-			} else {
-			this.cards.push(this.editedItem)
-			}
-			this.close()
-		},
+		}
     },
   }
 </script>
