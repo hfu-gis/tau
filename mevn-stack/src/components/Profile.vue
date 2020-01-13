953a06cd898390bf10aee116c5d05fb813c9b95f
TroubleBeast <oezkanyanikbas@gmail.com>
2020-01-13 15:47:49 +0100

    Change Profile UI

--- mevn-stack/src/components/Profile.vue
+++ mevn-stack/src/components/Profile.vue
@@ -1,65 +1,27 @@
 <template>
-    <main class='mt-8'>
-        <v-card
-            v-if="!isLoading"
-            class="mx-auto"
-            max-width="434"
-            tile>
-            <v-img
-                    height="100%"
-                    src="https://marmotamaps.com/de/fx/wallpaper/download/alpenberge/Marmotamaps_Wallpaper_MonteViso_Desktop_1920x1080.jpg"
-            >
-                <v-row
-                        align="end"
-                        class="fill-height"
-                >
-                    <v-col
-                            align-self="start"
-                            class="pa-0"
-                            cols="12"
-                    >
-                        <v-list-item-avatar size="130">
-                            <img src="https://randomuser.me/api/portraits/women/81.jpg">
-                        </v-list-item-avatar>
-                    </v-col>
-                    <v-col class="py-0">
-                        <v-list-item
-                                color="rgba(0, 0, 0, .4)"
-                                dark
-                        >
-                            <v-list-item-content>
-                                <v-list-item-title class="title">{{ user.email }}</v-list-item-title>
-                                <v-list-item-subtitle>{{ user.degree.stringValue }}</v-list-item-subtitle>
-                                <v-list-item-subtitle>{{ user.semester.integerValue }}.Semester</v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-list-item>
-                    </v-col>
-                </v-row>
-            </v-img>
-            <v-list subheader >
-                <v-subheader>Statistik</v-subheader>
-
-                <v-list-item
-                        v-for="item in items"
-                        :key="item.title"
-                >
-                    <v-list-item-icon>
-                        <v-icon>{{ item.icon }}</v-icon>
-                    </v-list-item-icon>
-
-                    <v-list-item-content>
-                        <v-list-item-title v-text="item.title"></v-list-item-title>
-                    </v-list-item-content>
-
-                    <v-list-item-content>
-                        <v-list-item v-text="item.number"></v-list-item>
-                    </v-list-item-content>
-
-                </v-list-item>
-            </v-list>
+    <main 
+        v-if="!isLoading" 
+        class='mt-8'>
+        <section>
+            <v-card tile class='pa-4 ma-4'>
+                <article>
+                    <header>
+                        <h2 class="title">E-Mail: {{ user.email }}</h2>
+                    </header>
+                    <v-divider class='mt-4 mb-4'></v-divider>
+                    <div class='text-center'>
+                        <v-chip 
+                            class='ma-1'
+                            color="green"
+                            text-color="white">{{ user.degree.stringValue }}</v-chip>
+                        <v-chip
+                            class='ma-1'
+                            color="green"
+                            text-color="white">{{ user.semester.integerValue }}. Semester</v-chip>
+                    </div>
+                </article>
+            </v-card>
             <v-btn
-                depressed
-                block
                 tile
                 @click="logout"
                 color='error'>
@@ -68,7 +30,8 @@
                     class="pr-2">mdi-logout</v-icon>
                 Logout
             </v-btn>
-        </v-card>
+        </section>
+    
         <v-text-field v-if='isLoading' color="success" loading></v-text-field>
     </main>
 </template>
@@ -76,6 +39,7 @@
 
 <script>
     import firebase, { firestore } from 'firebase'
+
     export default {
         name: "Profile",
         data: () => ({
@@ -85,24 +49,25 @@
 				email:    '',
 				id:       '',
 				degree:   '',
-				semester: ''
+                semester: ''
 			}
         }),
+        
         created() {
             this.isLoading = true
-			this.items = require('../assets/data/profile')
 
 			const user = firebase.auth().currentUser
 			this.user.email = user.email
 			this.user.id    = user.uid
 			let ref = firestore().collection('users').doc(this.user.id)
-			ref.get().then(result => {
-				let fields         = result._document.proto.fields
+            ref.get().then(doc => {
+				let fields         = doc._document.proto.fields
 				this.user.degree   = fields.degree
 				this.user.semester = fields.semester
-                this.isLoading = false
+                this.isLoading     = false
             })
-		},
+        },
+        
 		methods: {
 			logout () {
                 firebase.auth().signOut()
@@ -113,5 +78,19 @@
 </script>
 
 <style scoped>
-
+    section {
+        width: 90%;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    @media (min-width: 600px) {
+        section {
+            width: 60%
+        }
+    }
+    @media (min-width: 900px) {
+        section {
+            width: 30%
+        }
+    }
 </style>
